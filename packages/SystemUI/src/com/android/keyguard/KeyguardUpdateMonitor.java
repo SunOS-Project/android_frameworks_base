8d8343b2f7fe112878737ed71f74299a3b4f100d
Pranav Vashi <neobuddy89@gmail.com>
2021-06-26 09:48:39 +0530

    BatteryService: Add support for oem fast charger detection
    This is based on commit by amartinz:
    https://review.lineageos.org/c/LineageOS/android_frameworks_base/+/207583
    
    This supports dash, warp, vooc, turbo charging.
    
    Added config_oemFastChargerStatusPath and config_oemFastChargerStatusPath2
    as OEM can provide separate paths for wired and wireless fast charging.
    
    Change-Id: If9eeda0fa65666b39fa226c9c0083b6917810561
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -2480,7 +2480,7 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, CoreSt
             // Don't override if a valid battery status update has come in
             final BatteryStatus status = new BatteryStatus(BATTERY_STATUS_UNKNOWN,
                     /* level= */ level, /* plugged= */ 0, CHARGING_POLICY_DEFAULT,
-                    /* maxChargingWattage= */0, /* present= */true);
+                    /* maxChargingWattage= */0, /* present= */true, false);
             mMainExecutor.execute(() -> {
                 if (mBatteryStatus == null) {
                     handleBatteryUpdate(status);
@@ -3803,6 +3803,11 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, CoreSt
 
         // change in charging current while plugged in
         if (nowPluggedIn && current.maxChargingWattage != old.maxChargingWattage) {
+            return true;
+        }
+
+        // change in OEM charging while plugged in
+        if (nowPluggedIn && current.oemChargeStatus != old.oemChargeStatus) {
             return true;
         }
 

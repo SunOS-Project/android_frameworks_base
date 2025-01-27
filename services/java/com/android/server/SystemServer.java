f56accb18ea48b6863b9141ac014d52314e87dce
SamarV-121 <samar@samarv121.dev>
2025-01-27 20:12:38 +0800

    PIHooks: Dynamically spoof props for GMS
    neobuddy89: Adapted for PixelPropsUtils. Use local props as fallback.
    
    [clarencelol: Adapt changes from CrDroid.]
    
    Change-Id: I3a7266947e9b7eee9738807448a2f2babaf819e9
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: clarencelol <clarencekuiek@proton.me>

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -217,6 +217,7 @@ import com.android.server.pdb.PersistentDataBlockService;
 import com.android.server.people.PeopleService;
 import com.android.server.permission.access.AccessCheckingService;
 import com.android.server.pinner.PinnerService;
+import com.android.server.sun.AttestationService;
 import com.android.server.pm.ApexManager;
 import com.android.server.pm.ApexSystemServiceInfo;
 import com.android.server.pm.BackgroundInstallControlService;
@@ -2863,6 +2864,11 @@ public final class SystemServer implements Dumpable {
                 mSystemServiceManager.startService(BackgroundInstallControlService.class);
                 t.traceEnd();
             }
+
+            // AttestationService
+            t.traceBegin("AttestationService");
+            mSystemServiceManager.startService(AttestationService.class);
+            t.traceEnd();
         }
 
         t.traceBegin("StartMediaProjectionManager");
